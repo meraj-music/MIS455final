7e6eeeea92f0013773b1b9a75dd7aa13598b5a59
Meraj Mohammed Bhuiyan <meraj.mohammed99@gmail.com>
2022-04-22 10:02:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function(){
 
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=typedtext)
+.then(res=>res.json())
+.then(data=> displayFood(data))
+
+const displayFood = foods =>{
+const foodItemsDiv = document.getElementById('food-items'); 
+
+foods.meals.forEach(meal=>{
+
+    const foodDiv = document.createElement('div');
+
+    foodDiv.className = 'meal';
+    const foodInfo = `
+    <h3>${meal.strMeal}</h3>
+
+    `;
+    foodDiv.innerHTML = foodInfo;
+    foodItemsDiv.appendChild(foodDiv);
+});
+}
+}();
